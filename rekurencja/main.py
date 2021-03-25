9b31b639b59a7629ab71cb8e4f409bf622c3bd72
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2021-03-25 23:51:10 +0100

    init

--- rekurencja/main.py
+++ rekurencja/main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import bs4
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+exampleFile = open('example.html')
+exampleSoup = bs4.BeautifulSoup(exampleFile.read(), "html.parser")
+elems = exampleSoup.select('#author')
+print(type(elems)) # check what type is bs4 element
 
+len(elems) # check length of bs4
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+type(elems[0])
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+elems[0].getText()
