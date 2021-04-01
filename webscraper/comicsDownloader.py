5a1526857f3c6e8bc1b1ac8a70fa57b6c70d8ecc
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2021-04-01 08:26:37 +0200

    Adding working solution to xkcd-downloader.

--- webscraper/comicsDownloader.py
+++ webscraper/comicsDownloader.py
@@ -19,21 +19,21 @@ while not url.endswith('#'):
     else:
         try:
             comicUrl = 'http:' + comicElem[0].get('src')
-    # TODO: Download the image.
+    #   Download the image.
             print('Downloading image %s...' % (comicUrl))
             res = requests.get(comicUrl)
             res.raise_for_status()
         except requests.exceptions.MissingSchema:
-            # skip this comic
+    # skip this comic
             prevLink = soup.select('a[rel="prev"]')[0]
             url = 'http://xkcd.com' + prevLink.get('href')
             continue
-    # TODO: Save the image to ./xkcd.
+    #   Save the image to ./xkcd.
             imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)),'wb')
             for chunk in res.iter_content(100000):
                 imageFile.write(chunk)
             imageFile.close()
-    # TODO: Get the Prev button's url.
+    #   Get the Prev button's url.
             prevLink = soup.select('a[rel="prev"]')[0]
             url = 'https://xkcd.com' + prevLink.get('href')
 
