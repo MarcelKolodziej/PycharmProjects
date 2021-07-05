7976b8b602af8de5af2432e0642cb43ae8f7092e
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2021-07-05 16:26:40 +0200

    12 Python Projects Hangman problem with lives

---
+++ Practice_12_Projects/Paper_Rock_Scissors/main.py
@@ -0,0 +1,24 @@
+'''basic rock, paper, scissor game vs computer'''
+import random
+
+def play():
+    user = input("Type 'r' for rock, 'p' for paper, and 's' for scissors")
+    computer = random.choice(['s','r','p'])
+
+    if user == computer:
+        return 'It\'s a tie'
+
+    # r > s, s > p, p > r
+    if is_win(user, computer):
+        return 'You won'
+
+    def is_win(player, opponent):
+        # return true if player wins
+        # r > s, s > p, p > r
+        if(player == 'r' and opponent == 's' ) or (player == 's' and opponent == 'p') \
+        or (player == 'p' and opponent == 'r'):
+            return True
+
+print(play())
+
+
