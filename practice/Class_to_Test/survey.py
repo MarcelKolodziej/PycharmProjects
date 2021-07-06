2dffb20a520734b40918d7652fed09f3d6929af3
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2021-07-07 00:10:38 +0200

    survey added test for multiple responses

--- practice/Class_to_Test/survey.py
+++ practice/Class_to_Test/survey.py
@@ -1,8 +1,8 @@
 class AnonymousSurvey:
-    """Collect anonymous anserws to a survey question."""
+    """Collect anonymous answers to a survey question."""
 
     def __init__(self, question):
-        """Store a qeustion, and prepere to store responses."""
+        """Store a question, and prepare to store responses."""
         self.question = question
         self.responses = []
 
@@ -14,7 +14,7 @@ class AnonymousSurvey:
         """Store a single response to the survey."""
         self.responses.append(new_response)
 
-    def show_result(self):
+    def show_results(self):
         """Show all the responses that have been given."""
         print("Survey results:")
         for response in self.responses:
