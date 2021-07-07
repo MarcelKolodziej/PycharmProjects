e334051f2342989e3ff8631e8a60305a5bf910ee
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2021-07-07 18:44:36 +0200

    no message

--- practice/OOP_practice/oop.py
+++ practice/OOP_practice/oop.py
@@ -1,10 +1,15 @@
 # Object Orientated Programming in Python from youtube clip 'https://www.youtube.com/watch?v=JeznW_7DlB0'
 
 class Dog:
+    # special method
+    def __init__(self, name):
+        self.name = name
+        print(name)
+
+    def add_one(self, x):
+        return x + 1
 
     def bark(self):
         print("bark")
 
-d = Dog()
-d.bark()
-print(type(d))
+d = Dog("Tim")
