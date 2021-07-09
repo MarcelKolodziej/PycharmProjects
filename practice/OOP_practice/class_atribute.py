2aab8528b8805f2662a7b13494c1631a11832b02
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2021-07-09 11:28:31 +0200

    no message

--- practice/OOP_practice/class_atribute.py
+++ practice/OOP_practice/class_atribute.py
@@ -1,18 +1,4 @@
-class Person:
-    number_of_people = 1
+class Math:
 
-    def __init__(self, name):
-        self.name = name
-        Person.number_of_people += 1
-
-    @classmethod
-    def number_of_people_(cls):
-        return cls.number_of_people
-
-    @classmethod
-    def add_person(cls):
-        cls.number_of_people += 1
-
-p1 = Person("tim")
-p2 = Person("jill")
-print(Person.number_of_people_())
+    @staticmethod
+    def add5():
