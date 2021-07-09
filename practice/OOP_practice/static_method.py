8016d29055eb75065114113bd6b5ab16880cb2e6
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2021-07-09 12:19:28 +0200

    Tic-Tac-Toe game

--- practice/OOP_practice/static_method.py
+++ practice/OOP_practice/static_method.py
@@ -6,7 +6,12 @@ class Math:
 
 
     @staticmethod
-    def add5(x):
-        return x + 5
+    def add10(x):
+        return x + 10
 
-print(Math.add5(5))
+    @staticmethod
+    def pr():
+        print("run")
+
+
+Math.pr()
