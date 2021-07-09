924121b91bc7ae53f2bf70d9b4e83e3e039364a5
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2021-07-09 11:34:03 +0200

    class method, static method

--- practice/OOP_practice/class_atribute.py
+++ practice/OOP_practice/class_atribute.py
@@ -1,4 +1,24 @@
-class Math:
+# Function and method arguments:
 
-    @staticmethod
-    def add5():
+# Always use self for the first argument to instance methods.
+# Always use cls for the first argument to class methods.
+
+class Person:
+    number_of_people = 1
+
+    def __init__(self, name):
+        self.name = name
+        Person.number_of_people += 1
+
+    @classmethod
+    def number_of_people_(cls):
+        return cls.number_of_people
+
+    @classmethod
+    def add_person(cls):
+        cls.number_of_people += 1
+
+
+p1 = Person("tim")
+p2 = Person("jill")
+print(Person.number_of_people_())
