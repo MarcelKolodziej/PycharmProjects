145b88e42fd99bdbbdb45acf9cf8e3775b2518f4
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2021-07-30 01:18:20 +0200

    Modifying an Attribute's Value Throught a Method

--- practice/crash_course/classes/Car.py
+++ practice/crash_course/classes/Car.py
@@ -1,14 +1,28 @@
 class Car:
     """A simple attempt to represent a car."""
-    def __init__(self, make, model, year):
+    def __init__(self, make, model, year,):
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
-    def get_descreptive_name(self):
+    def get_descriptive_name(self):
         """Return neatly formatted descriptive name."""
-        long_name = f"{self.make=} {self.model} {self.year}"
+        long_name = f"{self.make} {self.model} {self.year}"
         return long_name.title()
 
-my_new_car = Car('Audi', 'a4', 2019)
-print(my_new_car.get_descreptive_name())
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer_reading(self, mileage):
+        """Set the odometer reading to the given value."""
+        self.odometer_reading = mileage
+
+my_new_car = Car('Audi', 'a4', 2019,)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.update_odometer_reading(25)
+# my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
